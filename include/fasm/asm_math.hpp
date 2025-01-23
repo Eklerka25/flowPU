52d189c275ba746f394b7c564bc4928123052c87
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-23 21:24:25 +0100

    Math header

---
+++ include/fasm/asm_math.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+#include <macros.hpp>
+#include <fasm/asm_mem.hpp>
+
+int16_t math_inc(int16_t value);
+int16_t math_add(int16_t adn, int16_t add);
+int16_t math_sub(int16_t min, int16_t sub);
+int16_t math_mlp(int16_t mld, int16_t mlr);
+int16_t math_div(int16_t dvr, int16_t did);
