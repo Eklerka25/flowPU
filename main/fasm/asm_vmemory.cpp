88d5015b233f769418ad6830dee22956813d6ced
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-25 15:43:59 +0100

    Video memory C++ file

---
+++ main/fasm/asm_vmemory.cpp
@@ -0,0 +1,32 @@
+#include <cstdint>
+#include <macros.hpp>
+#include <typedefs.hpp>
+
+#define VMEM_SIZE 16
+
+i8 vmem[VMEM_SIZE][2];
+
+void vmem_init(i8 value) {
+    for (int x = 0; x < VMEM_SIZE; x++) {
+        vmem[x][0] = value;
+        vmem[x][1] = value;
+    }
+}
+
+int vmem_write(int index, i8 char_value, i8 color_value) {
+    if (index < 16 && index >= 0) {
+        vmem[index][0] = char_value; 
+        vmem[index][1] = color_value; 
+    }
+}
+
+i8 vmem_read(int index, int btype) {
+    return vmem[index][btype];
+}
+
+void vmem_dump() {
+    for (int x = 0; x < 8; x++) {
+        vmem[x][0] = 0b00000000;
+        vmem[x][1] = 0b00000000;
+    }
+}
