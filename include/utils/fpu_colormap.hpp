e95681f2783af0d5c397cab895e425a841e113f5
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-26 10:57:45 +0100

    colors enum

---
+++ include/utils/fpu_colormap.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+enum fsvm_colors {
+    RESET = 0,
+    FG_BLACK = 90,
+
+    FG_RED = 31,
+    FG_GREEN = 32,
+    FG_YELLOW = 33,
+    FG_BLUE = 34,
+    FG_MAGENTA = 35,
+    FG_CYAN = 36,
+    FG_GREY = 37,
+
+    FG_LIGHT_RED = 91,
+    FG_LIGHT_GREEN = 92,
+    FG_LIGHT_YELLOW = 93,
+    FG_LIGHT_BLUE = 94,
+    FG_LIGHT_MAGENTA = 95,
+    FG_LIGHT_CYAN = 96,
+    FG_WHITE = 97,
+};
