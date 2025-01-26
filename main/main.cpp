3199407268a3b455a377aa492d42279f6d070896
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-26 10:18:38 +0100

    Hello, world! using printchar

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <cstdint>
 
+#include <fasm/asm_vmemory.hpp>
 #include <fasm/asm_mem.hpp>
 #include <fasm/asm_math.hpp>
 #include <fasm/asm_logic.hpp>
+#include <fasm/asm_llc.hpp>
+
+#include <fconsole/fconsole.hpp>
 
 int main() {
-    mem_write(0, math_inc(0b11));
-    write << mem_read(0);
+    mem_init(0b0000000000000000);
+    vmem_init(0b11111111);
+    system("title FlowPU emulator");
+    system("MODE CON:cols=80 lines=25");
+
+    while (true) { 
+        fconsole_printchar(74, 91);
+        fconsole_printchar(45, 91);
+        fconsole_printchar(52, 91);
+        fconsole_printchar(52, 91);
+        fconsole_printchar(55, 91);
+        fconsole_printchar(27, 91); 
+        fconsole_printchar(0, 91);
+        fconsole_printchar(63, 91);
+        fconsole_printchar(55, 91);
+        fconsole_printchar(58, 91);
+        fconsole_printchar(52, 91);
+        fconsole_printchar(44, 91);
+        fconsole_printchar(12, 91);
+
+        fconsole_refresh();
+        llc_pause();
+    }
 }
+
