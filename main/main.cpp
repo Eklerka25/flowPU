98a002a5ed1bd66b946d87dbfc88315d83971c27
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-26 10:53:00 +0100

    New simple hello world

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,7 @@
 #include <fasm/asm_llc.hpp>
 
 #include <fconsole/fconsole.hpp>
+#include <utils/fpu_colormap.hpp>
 
 int main() {
     mem_init(0b0000000000000000);
@@ -16,19 +17,19 @@ int main() {
     system("MODE CON:cols=80 lines=25");
 
     while (true) { 
-        fconsole_printchar(74, 91);
-        fconsole_printchar(45, 91);
-        fconsole_printchar(52, 91);
-        fconsole_printchar(52, 91);
-        fconsole_printchar(55, 91);
-        fconsole_printchar(27, 91); 
-        fconsole_printchar(0, 91);
-        fconsole_printchar(63, 91);
-        fconsole_printchar(55, 91);
-        fconsole_printchar(58, 91);
-        fconsole_printchar(52, 91);
-        fconsole_printchar(44, 91);
-        fconsole_printchar(12, 91);
+        fconsole_printchar(74, FG_MAGENTA);
+        fconsole_printchar(45, FG_LIGHT_MAGENTA);
+        fconsole_printchar(52, FG_MAGENTA);
+        fconsole_printchar(52, FG_LIGHT_MAGENTA);
+        fconsole_printchar(55, FG_MAGENTA);
+        fconsole_printchar(27, FG_LIGHT_MAGENTA); 
+        fconsole_printchar(00, FG_MAGENTA);
+        fconsole_printchar(63, FG_LIGHT_MAGENTA);
+        fconsole_printchar(55, FG_MAGENTA);
+        fconsole_printchar(58, FG_LIGHT_MAGENTA);
+        fconsole_printchar(52, FG_MAGENTA);
+        fconsole_printchar(44, FG_LIGHT_MAGENTA);
+        fconsole_printchar(12, FG_MAGENTA);
 
         fconsole_refresh();
         llc_pause();
