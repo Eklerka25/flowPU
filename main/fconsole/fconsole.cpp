b0cd93eadd2c6270c6af68ef8f67a96ed063d01d
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-26 10:17:39 +0100

    [FCONSOLE] Color byte and extended screen size (80x25)

--- main/fconsole/fconsole.cpp
+++ main/fconsole/fconsole.cpp
@@ -12,9 +12,9 @@ int cursor_pos_y = 0;
 void fconsole_refresh() {
     llc_vmode_clear();
     int a = 0;
-    for (int x = 0; x < 4; x++) {
-        for (int y = 0; y < 4; y++) {
-            write << charmap_return(vmem_read(a, 0));
+    for (int x = 0; x < 24; x++) {
+        for (int y = 0; y < 80; y++) {
+            write << "\033[" << vmem_read(a, 1) << "m" << charmap_return(vmem_read(a, 0));
             a++;
         }
         write << "\n";
@@ -25,8 +25,8 @@ void fconsole_write(int x, int y, int index) {
     vmem_write(x, index, 0b0);
 }
 
-void fconsole_printchar(int index) {
-    vmem_write(cursor_pos_x, index, 0b0);
+void fconsole_printchar(int index, int color) {
+    vmem_write(cursor_pos_x, index, color);
     cursor_pos_x++;
-    vmem_write(cursor_pos_x, 24, 0b0);
+    vmem_write(cursor_pos_x, 24, 0x61);
 }
