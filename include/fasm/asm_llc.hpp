041b24e3ada8d2ec6aeac7d4667d23a8f13d25aa
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-29 15:01:59 +0100

    Updated LLC header

--- include/fasm/asm_llc.hpp
+++ include/fasm/asm_llc.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <conio.h>
 #include <cstdint>
 #include <macros.hpp>
 #include <typedefs.hpp>
+
+#include <utils/fpu_charmap.hpp>
 
 void llc_halt();
 void llc_exit();
 void llc_pause();
 void llc_vmode_clear();
+int llc_getchar();
